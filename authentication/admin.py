bf49360d02d44d00076feefb4b18144927a2807c
Nahid <nahidthedeveloper@gmail.com>
2024-11-14 19:01:51 +0600

    add permission model in django

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from authentication.models import Account
+from django.contrib.auth.models import Permission
 
 admin.site.register(Account)
+admin.site.register(Permission)
